c0b6b9b4074d0fe652c780583c43614407553683
khayato1009 <hayato8810k@icloud.com>
2023-03-30 11:06:44 +0900

    モデルとテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_28_064209) do
+ActiveRecord::Schema.define(version: 2023_03_29_071022) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "area_of_origin_id", null: false
+    t.integer "estimated_sipping_date_id", null: false
+    t.integer "selling_price", null: false
+    t.string "name", null: false
+    t.text "detail", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2023_03_28_064209) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
