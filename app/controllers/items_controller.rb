a560147b650a4741f5a6e454aa377f90983c35e5
khayato1009 <hayato8810k@icloud.com>
2023-04-03 16:55:36 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit]
+  before_action :move_to_edit, only: :edit
+  
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -16,14 +24,25 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
-  def edit
-    
-  end
-
+  
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+ 
   
   private
 
@@ -31,4 +50,15 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :category_id, :condition_id, :shipping_cost_id, :area_of_origin_id,
                                  :estimated_sipping_date_id, :selling_price, :name, :detail).merge(user_id: current_user.id)
   end
+
+  
+ 
+
+
+  def move_to_edit
+    if current_user.id != @item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
