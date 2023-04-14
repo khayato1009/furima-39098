2e4f86e24e52022769ae7b10d496eb5e8f3d8862
khayato1009 <hayato8810k@icloud.com>
2023-04-14 12:09:34 +0900

    編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock ' 3.17.2'
 set :application, 'furima-39098'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:k.hayato1009/furima-39098.git'
+set :repo_url,  'git@github.com:khayato1009/furima-39098.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
